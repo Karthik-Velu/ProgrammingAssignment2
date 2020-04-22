c274415de493decf6f06231674ecc41ac41236fa
Karthik-Velu <kaartik.velu@gmail.com>
2020-04-23 03:20:17 +0530

    first and final commit - assignment ready for submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Overall design of the code is similar to the structure used to cache mean value in the assignment question 
+##by using lexical scoping to cache the result of inverse caculations the first time the cachesolve function 
+##is called on an object of makeCacheMatrix. subsequent calls to cachesolve function for the same object retrieves 
+##cache data  
 
-## Write a short comment describing this function
+##function returns a list of 4 functions - getter and setter for the input matrix(i.e. X) and inverse matrix (i.e. IM)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im <- NULL
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setim <- function(inv) im <<- inv
+  getim <- function() im
+  list(set = set, get = get,
+       setim = setim,
+       getim = getim)
 }
 
 
-## Write a short comment describing this function
+## function which actually calculates the inverse by calling solve function for the first time while simeltaneously
+## setting im value in makeCacheMatrix function and subsequent calls using same object - the im value will be retreived
+## using getim function from makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  im <- x$getim()
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data)
+  x$setim(im)
+  im
 }
